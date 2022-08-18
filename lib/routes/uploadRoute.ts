05da608c7fab82bcf96aac9327ac789c6d255dc9
eyong-joel7 <eyong.joel@gmail.com>
2022-08-18 06:16:37 +0300

    feat: added file type validation on profile image upload

--- lib/routes/uploadRoute.ts
+++ lib/routes/uploadRoute.ts
@@ -1,11 +1,11 @@
 import { UploadController } from '../controllers/uploadController';
 import { Application, NextFunction, Request, Response } from 'express';
-import multer, { StorageEngine } from 'multer';
+import multer, { FileFilterCallback, StorageEngine } from 'multer';
 import AuthenticationMiddleWare from '../middlewares/auth';
 import CommonService from '../modules/common/service';
 
 export class UploadRoutes {
-  private maxFileSize: number = 1 * 1024 * 1024; // Max file size is 5MB
+  private maxFileSize: number = 5 * 1024 * 1024; // Max file size is 5MB
   private uploadController: UploadController = new UploadController();
   private storage: StorageEngine = multer.diskStorage({
     destination(req, file, cb) {
@@ -16,9 +16,16 @@ export class UploadRoutes {
     },
   });
 
-  public upload = multer({ storage: this.storage, limits: { fileSize: this.maxFileSize } }).single(
-    'image'
-  );
+  public upload = multer({
+    storage: this.storage,
+    limits: { fileSize: this.maxFileSize },
+    fileFilter(req, file, callback: FileFilterCallback) {
+      if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return callback(null, false);
+      }
+      callback(null, true);
+    },
+  }).single('image');
 
   public route(app: Application) {
     app.post(
